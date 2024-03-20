1501a771ecca9e914289438b22eec3d818418d03
shaival-vora <shaivalvora@gmail.com>
2024-03-20 14:59:31 -0400

    Added the Network service class to make the API call using Async Await

--- FetchAssessment/FetchAssessment/Service/ItemNetworkService.swift
+++ FetchAssessment/FetchAssessment/Service/ItemNetworkService.swift
@@ -7,6 +7,42 @@
 
 import Foundation
 
+// Network Service Structure: It promotes a clean separation of concerns by isolating networking code from SwiftUI views and view models, thus improving code readability, maintainability, and testability.
+
+/// - ItemNetworkService class makes the API call to get the details about the hiringAPI data
 struct ItemNetworkService {
+    
+    /// Enum to represent different errors that can occur during network requests.
+    enum ItemError: Error {
+        case failed
+        case failedToDecode
+        case invalidStatusCode
+    }
+    
+    /// Function to fetch item data asynchronously from the API.
+    func fetchItemData() async throws -> [ItemModel] {
+        //create url from the URL string
+        let defaultUrlString: String = "https://fetch-hiring.s3.amazonaws.com/hiring.json"
+      
+        guard let url = URL(string: defaultUrlString) else {
+            throw ItemError.failed
+        }
+        
+        // Fetch the data from the API
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        // Check if the response status code is valid (200).
+        guard let response = response as? HTTPURLResponse,
+              response.statusCode == 200 else {
+            throw ItemError.invalidStatusCode
+        }
+        
+        // decode the data using JSON decoder
+        if let decodedResponse = try? JSONDecoder().decode([ItemModel].self, from: data) {
+            return  decodedResponse
+        } else {
+            throw ItemError.failedToDecode
+        }
+    }
     
 }
