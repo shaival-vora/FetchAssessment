c1f2ef43b8228e1abe1410d28359244d44fd3ab3
shaival-vora <shaivalvora@gmail.com>
2024-03-20 11:50:35 -0400

    Initial Project structure commit

---
+++ FetchAssessment/FetchAssessment/FetchAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAssessmentApp.swift
+//  FetchAssessment
+//
+//  Created by Shaival Vora on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
