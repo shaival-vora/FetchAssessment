8318aa26eb5ac3d4ea53518cd7a1fef9602fd9f7
shaival-vora <shaivalvora@gmail.com>
2024-03-20 15:01:11 -0400

    Content View file Will display the data given from the View Model

--- FetchAssessment/FetchAssessment/ContentView.swift
+++ FetchAssessment/FetchAssessment/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ItemViewModel(service: ItemNetworkService())
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Switch on ViewModel state For now I have kept a progressview for loading state which is when the API call is being made and on success state it will display the list as required and I have kept a Empty view for rest of the states
+            switch viewModel.state {
+            case .loading:
+                // If ViewModel is in loading state, show ProgressView
+                ProgressView()
+            case .success(let itemData):
+                // If ViewModel is in success state, display fetched data in a List
+                List {
+                    ForEach(itemData.sorted(by: {$0.key < $1.key}), id: \.key) { key, items in
+                        Section(header: Text("List ID: \(key), Item count: \(items.count)")) {
+                            ForEach(items, id: \.id) { item in
+                                Text("ID: \(item.id), Name: \(item.name ?? "")")
+                            }
+                        }
+                    }
+                }
+            default:
+                // If ViewModel is in any other state, display EmptyView
+                EmptyView()
+            }
+
         }
-        .padding()
+        .task { // Asynchronously fetch item data when the view appears
+            await viewModel.getItemData()
+        }
     }
+    
 }
 
 #Preview {
